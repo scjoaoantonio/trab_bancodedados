00e9fed4b36a3437b7c2762bc0d72c0f1ca5c2c6
João Antônio <65183458+scjoaoantonio@users.noreply.github.com>
2023-11-02 17:51:37 -0300

    Update data.py

--- app/data.py
+++ app/data.py
@@ -1,11 +1,11 @@
 import sqlite3
 
 def import_sqlite_database():
-    # Conecte-se ao banco de dados ou crie um novo se ele não existir
+    # conectar a um bd ou criar um do zero se nao existir nenhum
     conn = sqlite3.connect('data/campeonato.db')
     cursor = conn.cursor()
 
-    # Abra o arquivo .sql e execute o script SQL
+    # abrir o arquivo sql e executar o script
     with open('data/database.sql', 'r') as script_file:
         script = script_file.read()
         cursor.executescript(script)
@@ -13,10 +13,6 @@ def import_sqlite_database():
     # Commit as alterações e feche a conexão com o banco de dados
     conn.commit()
     conn.close()
-
-def create_tables():
-    # Esta função não é mais necessária, pois o banco de dados foi importado usando import_sqlite_database()
-    pass
 
 def insert_time(nome, gols_sofridos, gols_marcados, pontos, vitorias, derrotas, partidas_jogadas, empates, treinador, campeonato_ano):
     conn = sqlite3.connect('data/campeonato.db')
@@ -28,7 +24,6 @@ def insert_time(nome, gols_sofridos, gols_marcados, pontos, vitorias, derrotas, 
     conn.commit()
     conn.close()
 
-# Função para excluir um time do banco de dados
 def delete_time(time_name):
     conn = sqlite3.connect('data/campeonato.db')
     cursor = conn.cursor()
@@ -67,16 +62,6 @@ def insert_time(nome, gols_sofridos, gols_marcados, pontos, vitorias, derrotas, 
     cursor.execute("INSERT INTO Time (Nome, GolsSofridos, GolsMarcados, Pontos, Vitórias, Derrotas, PartidasJogadas, Empates, Treinador, CampeonatoAno) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                    (nome, gols_sofridos, gols_marcados, pontos, vitorias, derrotas, partidas_jogadas, empates, treinador, campeonato_ano))
 
-    conn.commit()
-    conn.close()
-
-# Função para excluir um time do banco de dados
-def delete_time(time_name):
-    conn = sqlite3.connect('data/campeonato.db')
-    cursor = conn.cursor()
-
-    cursor.execute("DELETE FROM Time WHERE Nome = ?", (time_name,))
-    
     conn.commit()
     conn.close()
 
